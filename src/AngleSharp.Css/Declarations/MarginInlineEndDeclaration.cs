fc63b24f05a31088873f9947b2844504a53fe95f
jason.bray@faithlife.com <jason.bray@faithlife.com>
2023-05-30 16:45:59 -0400

    Add support for inline and block margin and padding properties. CON-479

---
+++ src/AngleSharp.Css/Declarations/MarginInlineEndDeclaration.cs
@@ -0,0 +1,22 @@
+﻿namespace AngleSharp.Css.Declarations
+{
+    using System;
+    using Dom;
+    using static ValueConverters;
+
+    public class MarginInlineEndDeclaration
+    {
+        public static String Name = PropertyNames.MarginInlineEnd;
+
+        public static String[] Shorthands = new[]
+        {
+            PropertyNames.MarginInline,
+        };
+
+        public static IValueConverter Converter = AutoLengthOrPercentConverter;
+
+        public static ICssValue InitialValue = InitialValues.MarginInlineEndDecl;
+
+        public static PropertyFlags Flags = PropertyFlags.Unitless | PropertyFlags.Animatable;
+    }
+}
---
+++ src/AngleSharp.Css/Declarations/MarginInlineStartDeclaration.cs
@@ -0,0 +1,22 @@
+﻿namespace AngleSharp.Css.Declarations
+{
+    using System;
+    using Dom;
+    using static ValueConverters;
+
+    public class MarginInlineStartDeclaration
+    {
+        public static String Name = PropertyNames.MarginInlineStart;
+
+        public static String[] Shorthands = new[]
+        {
+            PropertyNames.MarginInline,
+        };
+
+        public static IValueConverter Converter = AutoLengthOrPercentConverter;
+
+        public static ICssValue InitialValue = InitialValues.MarginInlineStartDecl;
+
+        public static PropertyFlags Flags = PropertyFlags.Unitless | PropertyFlags.Animatable;
+    }
+}
